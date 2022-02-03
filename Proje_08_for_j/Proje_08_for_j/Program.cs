1dbe0e55f5da590425690dec16ee9aa0b49c6bef
Web ve Mobil Öğle <Web ve Mobil le@DESKTOP-8M7D7GE>
2022-02-03 15:44:15 +0300

    Proje 8 i ekledik

---
+++ Proje_08_for_j/Proje_08_for_j/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proje_08_for_j
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
