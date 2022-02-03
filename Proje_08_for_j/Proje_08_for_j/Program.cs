76cee31af0d9b6fc006a7cc04f78e22337a3e436
Web ve Mobil Öğle <Web ve Mobil le@DESKTOP-8M7D7GE>
2022-02-03 18:03:11 +0300

    Proje 8 tamamlandı

--- Proje_08_for_j/Proje_08_for_j/Program.cs
+++ Proje_08_for_j/Proje_08_for_j/Program.cs
@@ -6,7 +6,151 @@ namespace Proje_08_for_j
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int toplam = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+
+            //    if (i % 2 == 0)
+            //    {
+            //        toplam += i;
+            //    }
+            //}
+            //Console.WriteLine(toplam);
+            //Console.ReadLine();
+
+            //int toplam = 0;
+            //for (int i = 2; i <= 10; i+=2)
+            //{
+
+
+            //        toplam += i;
+
+            //}
+            //Console.WriteLine(toplam);
+            //Console.ReadLine();
+
+
+            // bir ile yüz arasındaki asal sayıları ekrana yazdıran program
+
+
+
+            //int asalE;
+            //bool asal=false;
+
+            //for (int i = 2; i <= 100; i++)
+            //{
+            //    for (int j = 2; j < i; j++)
+            //    {
+            //        if (i % j != 0)
+            //        {
+            //            asal = true;
+            //            asalE = i;
+            //            Console.WriteLine(asalE);
+
+            //        }
+
+
+            //    }
+
+
+            //}
+            //Console.ReadLine();
+            //bool asal = true;
+
+            //for (int i = 2; i <= 100; i++)
+            //{
+
+            //    for (int j = 2; j < i; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            asal = false;
+            //            break;
+
+            //        }
+            //    }
+            //    if (asal)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    asal = true;
+
+            //}
+            //Console.ReadLine();
+
+            // bool asal = true;
+            //int sayac = 1, sayac2 = 1, sayac3 = 1;
+
+            // for (int i = 2; i <= 300; i++)
+            // {
+
+            //     for (int j = 2; j < i; j++)
+            //     {
+            //         if (i % j == 0)
+            //         {
+            //             asal = false;
+            //             break;
+
+            //         }
+            //     }
+            //     if (asal)
+            //     {
+
+            //         if (i < 100)
+            //         {
+            //             sayac++;
+
+
+            //         }
+
+            //        else if (i<=200)
+            //         {
+            //             sayac2++;
+
+            //         }
+            //         else 
+            //         {
+            //             sayac3++;
+
+            //         }
+
+            //     }
+            //     asal = true;
+
+            // }
+            // Console.WriteLine($"1-100 arasında {sayac} asal sayı var\n 101-200rasında {sayac2} asal sayı var\n201-300 arasında {sayac3} asal sayı var");
+
+
+            //Console.ReadLine();
+
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write(" * ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+            
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($" {i * j}\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+
         }
     }
 }
